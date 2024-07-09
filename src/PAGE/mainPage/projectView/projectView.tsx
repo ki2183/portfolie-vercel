4be3756c27074f70f3acb167dbd1aca708221de1
ki2183 <ki2183@naver.com>
2024-07-09 09:10:34 +0900

    life cycle update

--- src/PAGE/mainPage/projectView/projectView.tsx
+++ src/PAGE/mainPage/projectView/projectView.tsx
@@ -58,24 +58,19 @@ function ProjectView(){
     useGSAP(()=>{
 
         const scroll_setup_async = async()=>{
-            await new Promise<void>((resolve,reject)=>{
-                window.addEventListener('resize',()=>{
-                    const func = () =>{
-                    const Trigger1 = ScrollTrigger.getById("scroll-img");
-                    if(Trigger1)
-                        Trigger1.kill()
-                    const Trigger2 = ScrollTrigger.getById("scroll-line");
-                    if(Trigger2)
-                        Trigger2.kill()
-                    const Trigger3 = ScrollTrigger.getById("scroll-info");
-                    if(Trigger3)
-                        Trigger3.kill()
-                    }
-                    func()
-                    resolve();
-                })    
-            })
-            
+            const func = () =>{
+                const Trigger1 = ScrollTrigger.getById("scroll-img");
+                if(Trigger1)
+                    Trigger1.kill()
+                const Trigger2 = ScrollTrigger.getById("scroll-line");
+                if(Trigger2)
+                    Trigger2.kill()
+                const Trigger3 = ScrollTrigger.getById("scroll-info");
+                if(Trigger3)
+                    Trigger3.kill()
+                }
+     
+            await func()
             await scroll_setup_handler()
         } 
         scroll_setup_async()
